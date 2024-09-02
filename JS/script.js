508290a57b5fdb80c9f157b79e45a9658b5144ff
SEBonduel <sebastien050599@hotmail.fr>
2024-09-03 01:30:57 +0200

    fixed all

--- JS/script.js
+++ JS/script.js
@@ -26,3 +26,47 @@ closeButtons.forEach(button => {
         event.stopPropagation();
     });
 });
+
+// Sélectionne la checkbox du menu burger
+const burgerMenuCheckbox = document.getElementById('menu-toggle');
+
+// Sélectionne toutes les autres checkboxes (onglets)
+const otherCheckboxes = document.querySelectorAll('.onglet');
+
+// Variable pour stocker la dernière checkbox cochée
+let lastCheckedCheckbox = null;
+
+// Coche la première checkbox par défaut au début
+if (!document.querySelector('.onglet:checked')) {
+    otherCheckboxes[0].checked = true;
+    lastCheckedCheckbox = otherCheckboxes[0];
+}
+
+// Ajoute un événement sur chaque checkbox sauf celle du menu burger
+otherCheckboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', function() {
+        if (checkbox.checked) {
+            // Stocke la checkbox cochée
+            lastCheckedCheckbox = checkbox;
+        }
+    });
+});
+
+// Ajoute un événement sur la checkbox du menu burger
+burgerMenuCheckbox.addEventListener('change', function() {
+    if (burgerMenuCheckbox.checked) {
+        // Désélectionne toutes les autres checkboxes
+        otherCheckboxes.forEach(checkbox => {
+            checkbox.checked = false;
+        });
+    } else {
+        // Restaure la dernière checkbox cochée si elle existe
+        if (lastCheckedCheckbox) {
+            lastCheckedCheckbox.checked = true;
+        } else {
+            // Si aucune checkbox n'avait été cochée, coche la première checkbox par défaut
+            otherCheckboxes[0].checked = true;
+            lastCheckedCheckbox = otherCheckboxes[0];
+        }
+    }
+});
